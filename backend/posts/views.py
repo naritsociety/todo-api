e90777c98b7424067a44fc9e82b70710dd3dce07
Narit Charoenval <naritsociety@gmail.com>
2020-10-19 16:51:18 +0700

    Add router

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,20 +1,20 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly # NEW
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-
-    # permission_classes = (permissions.IsAuthenticated,) # NEW
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
 
     permission_classes = (IsAuthorOrReadOnly,)  # NEW
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer

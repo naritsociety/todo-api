e37c752c599369d50a7cd0c9f2de6396d84e3d76
Narit Charoenval <naritsociety@gmail.com>
2020-10-19 10:35:41 +0700

    Add Todo Model

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
 
-class ListTodo():
-    pass
+from .models import Todo
+from .serializers import TodoSerializer
 
 
-class DetailTodo():
-    pass
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
